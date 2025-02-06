d399cb9fe38c37270def5552899b700c1bf9f07d
EhrlerL <leonid@ehrler-web.de>
2025-02-06 11:58:22 +0100

    set up stage zoom on scroll

--- frontend/src/components/home/Stage.js
+++ frontend/src/components/home/Stage.js
@@ -1,4 +1,5 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
+import { motion, useScroll, useTransform } from 'framer-motion';
 
 import { Image } from 'react-bootstrap';
 
@@ -6,19 +7,49 @@ import ParallaxText from '../general/ParallaxText';
 
 import '../../assets/css/Stage.css';
 
-import carside from '../../assets/images/car-side.svg';
 import kevisual from '../../assets/images/kevisual.svg';
 import image41 from '../../assets/images/image-41.png';
+import stagecar from '../../assets/images/stage-car.svg';
+import stagebg from '../../assets/images/stage-bg.svg';
+import outlinecar from '../../assets/images/car-outlines.svg';
 
-function Stage({ image1, image2, image3, ...props }) {
+function Stage({ props }) {
+    const containerRef = useRef(null);
+    const { scrollYProgress } = useScroll({
+        target: containerRef,
+        offset: ["start start", "end start"],
+    });
+
+    // Move background left/right based on scroll direction
+    const bgX = useTransform(scrollYProgress, [0, 1], ["-100%", "-20%"]);
+
+    // Scale down foreground car slightly
+    const carScale = useTransform(scrollYProgress, [0, 0.2], [3, 1.5]);
 
     return (
-        <>
-        <div>
-            <div>
-                <Image src={image41} className="bgimg" fluid />
+        <>        
+        <div ref={containerRef} className="relative w-full h-[300vh]">
+            {/* Parallax Section - Sticky Effect */}
+            <motion.div
+                className="fixed top-0 left-0 h-screen w-full flex overflow-hidden"
+            >
+                {/* Background Image - Moves sideways */}
+                <motion.div className="absolute inset-0 z-0" style={{ x: bgX }}>
+                    <Image src={stagebg} alt="Background" className="bgimg w-full h-full object-cover" />
+                </motion.div>
+
+                {/* Foreground Car - Zooms out */}
+                <motion.div className="absolute inset-0 flex z-10" style={{ scale: carScale }}>
+                    <Image src={stagecar} alt="Car" className="carimg w-auto h-auto" />
+                </motion.div>
+            </motion.div>
+
+            {/* Outlined Car */}
+            <div className={`h-[200vh] transition-opacity duration-500`}>
+                <Image src={outlinecar} alt="Outlined Car" className="outlinecar" />
             </div>
         </div>
+
         <section className="my-5" style={{ whiteSpace: "nowrap" }}>
             <ParallaxText baseVelocity={-5}>
                 {/*<Image src={kevisual} style={{ height: "4rem" }} className="kevisual" />*/}
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,15 +1,18 @@
 import { Col, Container, Row } from 'react-bootstrap';
+import { useEffect, useRef, useState } from 'react';
+import { motion, useScroll, useTransform } from 'framer-motion';
 
 import Stage from '../components/home/Stage';
 import Intro from '../components/home/Intro';
 import '../assets/css/Stage.css';
 
 function Home({ props }) {
+    
 
     return (
         <>
-        <Stage />
-        <Intro />
+            <Stage />
+            <Intro />
         </>
     );
 }
